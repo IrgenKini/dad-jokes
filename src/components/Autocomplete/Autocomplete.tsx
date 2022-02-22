64094eb86ac30ba94b8b3f05b41a89dbd17e2cb6
Irgen Kini <irakini1@gmail.com>
2022-02-22 14:03:24 +0100

    behaviour modified

--- src/components/Autocomplete/Autocomplete.tsx
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -11,48 +11,70 @@ function Autocomplete({ suggestions, search, setSearch }: AutocompleteProps) {
   const [areSuggestionsVisible, setAreSuggestionsVisible] =
     React.useState(false);
 
+  const [selectedSuggestion, setSelectedSuggestion] =
+    React.useState<string>("");
+
+  const wrapperRef = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, [wrapperRef]);
+
+  const handleClickOutside = (event: any) => {
+    if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+      setAreSuggestionsVisible(false);
+    }
+  };
+
   return (
     <div className={styles.mainDiv}>
-      <div className={styles.inputDiv}>
-        <input
-          className={styles.inputClass}
-          value={search}
-          onChange={(e) => {
-            setSearch(e.target.value);
-            setAreSuggestionsVisible(true);
-          }}
-        />
-        <div className={styles.divDownButton}>
-          <button
-            onClick={() => setAreSuggestionsVisible(!areSuggestionsVisible)}
-            className={styles.downButton}
-          >
-            &#9660;
-          </button>
+      <div ref={wrapperRef} className={styles.refDiv}>
+        <div className={styles.inputDiv}>
+          <input
+            onFocus={(e) => e.target.select()}
+            className={styles.inputClass}
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+              setAreSuggestionsVisible(true);
+            }}
+          />
+          <div className={styles.divDownButton}>
+            <button
+              onClick={() => setAreSuggestionsVisible(!areSuggestionsVisible)}
+              className={styles.downButton}
+            >
+              &#9660;
+            </button>
+          </div>
         </div>
-      </div>
-      {areSuggestionsVisible && (
-        <div className={styles.suggestionsDiv}>
-          <ul className={styles.suggList}>
-            {suggestions &&
-              suggestions.length > 0 &&
-              suggestions.map((item, index) => (
-                <li
-                  key={index}
-                  className={styles.listItem}
-                  onClick={() => setSearch(item.joke)}
-                >
-                  {item.joke}
+        <div className={styles.suggSelectedDiv}>{selectedSuggestion}</div>
+        {areSuggestionsVisible && (
+          <div className={styles.suggestionsDiv}>
+            <ul className={styles.suggList}>
+              {suggestions &&
+                suggestions.length > 0 &&
+                suggestions.map((item, index) => (
+                  <li
+                    key={index}
+                    className={styles.listItem}
+                    onClick={() => setSelectedSuggestion(item.joke)}
+                  >
+                    {item.joke}
+                  </li>
+                ))}
+              {(!suggestions || suggestions.length === 0) && (
+                <li key={0} className={styles.listItem}>
+                  No suggestions{" "}
                 </li>
-              ))}
-            {(!suggestions || suggestions.length === 0) && (
-              <li key={0} className={styles.listItem}>
-                No suggestions{" "}
-              </li>
-            )}
-          </ul>
-        </div>
-      )}
+              )}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
